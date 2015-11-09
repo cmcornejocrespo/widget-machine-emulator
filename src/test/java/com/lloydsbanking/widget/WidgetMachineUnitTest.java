2db82db388b0d1f615aa50464b93ef62d4941164
Carlos M. Cornejo <ccornejo@efinancialcareers.com>
2015-11-09 09:52:23 +0000

    added missing tests

--- src/test/java/com/lloydsbanking/widget/WidgetMachineUnitTest.java
+++ src/test/java/com/lloydsbanking/widget/WidgetMachineUnitTest.java
@@ -1,6 +1,8 @@
 package com.lloydsbanking.widget;
 
+import com.lloydsbanking.engine.Engine;
 import com.lloydsbanking.engine.InternalCombustionEngine;
+import com.lloydsbanking.engine.SteamEngine;
 import com.lloydsbanking.fuel.FuelType;
 import org.junit.Test;
 
@@ -12,10 +14,10 @@ public class WidgetMachineUnitTest {
     private Widget sut;
 
     @Test
-    public void itShouldCalculateTheExpectedValue(){
+    public void itShouldReturnTheExpectedValueWhenUsingUsingInternalCombustionEngineAndPetrolFuel(){
 
         //test fixtures
-        InternalCombustionEngine engine = new InternalCombustionEngine(FuelType.PETROL);
+        Engine engine = new InternalCombustionEngine(FuelType.PETROL);
         engine.fill(50);
 
         //given
@@ -28,4 +30,54 @@ public class WidgetMachineUnitTest {
         assertThat(totalCost).isEqualTo(9.0);
     }
 
+    @Test
+    public void itShouldReturnTheExpectedValueWhenUsingInternalCombustionEngineAndDieselFuel(){
+
+        //test fixtures
+        Engine engine = new InternalCombustionEngine(FuelType.DIESEL);
+        engine.fill(100);
+
+        //given
+        sut = new WidgetMachine(engine);
+
+        //when
+        double totalCost = sut.produceWidgets(50);
+
+        //then
+        assertThat(totalCost).isEqualTo(84.0);
+    }
+
+    @Test
+    public void itShouldReturnTheExpectedValueWhenUsingSteamEngineAndWoodFuel(){
+
+        //test fixtures
+        Engine engine = new SteamEngine(FuelType.WOOD);
+        engine.fill(25);
+
+        //given
+        sut = new WidgetMachine(engine);
+
+        //when
+        double totalCost = sut.produceWidgets(30);
+
+        //then
+        assertThat(totalCost).isEqualTo(65.25);
+    }
+
+    @Test
+    public void itShouldReturnTheExpectedValueWhenUsingSteamEngineAndCoalFuel(){
+
+        //test fixtures
+        Engine engine = new SteamEngine(FuelType.COAL);
+        engine.fill(75);
+
+        //given
+        sut = new WidgetMachine(engine);
+
+        //when
+        double totalCost = sut.produceWidgets(80);
+
+        //then
+        assertThat(totalCost).isEqualTo(226.0);
+    }
 }
